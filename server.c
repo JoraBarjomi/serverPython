395953198078217c0824b86b8a67706718cee983
JoraBarjomi <vip.nero228@mail.ru>
2025-12-07 08:10:29 +0700

    Using zmq in server.c

--- server.c
+++ server.c
@@ -6,60 +6,43 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include "zmq.h"
+
 #define PORT 12345
+
 int main(int argc, char const* argv[])
 {
-    int server_fd, new_socket;
-    ssize_t valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    socklen_t addrlen = sizeof(address);
-    char buffer[1024] = { 0 };
-    char* hello = "Hello from server";
-
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    if (bind(server_fd, (struct sockaddr*)&address,
-             sizeof(address))
-        < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket
-         = accept(server_fd, (struct sockaddr*)&address,
-                  &addrlen))
-        < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-  
+    void* context = zmq_ctx_new();
+    void* respond = zmq_socket(context, ZMQ_REP);
+    
+    zmq_bind(respond, "tcp://*:12345");
     printf("Server running...\n");
     while(true){
-        valread = read(new_socket, buffer, 1024 - 1); 
-        printf("%s\n", buffer);
-        for(int i = 0; i < strlen(buffer); i++){
-            buffer[i] = toupper(buffer[i]);
+
+        zmq_msg_t request;
+        zmq_msg_init(&request);
+        zmq_msg_recv(&request, respond, 0);
+        char *data = (char *)zmq_msg_data(&request);
+        printf("%s\n", data);
+        zmq_msg_close(&request);
+
+        sleep(2);
+
+        zmq_msg_t reply;
+        zmq_msg_init_size(&reply, strlen(data));
+
+        char new_data[strlen(data) + 1];
+        for(int i = 0; i < strlen(data); i++){
+            new_data[i] = toupper(data[i]);
         }
-        send(new_socket, buffer, strlen(buffer), 0);
-        buffer [0]= '\0';
+        new_data[strlen(data)] = '\0';
+
+        memcpy(zmq_msg_data(&reply), new_data, strlen(new_data));
+        zmq_msg_send(&reply, respond, 0);
+        zmq_msg_close(&reply);
     }
     
-    close(new_socket);
-    close(server_fd);
+    zmq_close(respond);
+    zmq_ctx_destroy(context);
     return 0;
 }
